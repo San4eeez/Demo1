c6189ff498951fea93719c0670a12df30a10585d
San4eeez <AlexandroHell@yandex.ru>
2024-10-01 15:33:40 +0300

    cc

---
+++ Models/Demo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demka.Models;
+
+public partial class Demo
+{
+    public int Id { get; set; }
+
+    public string? Chtoto { get; set; }
+}
